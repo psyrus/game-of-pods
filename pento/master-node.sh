532e9e33db3329b09ebc1486fd6f01b123384f38
Psyrus <revolution.x@gmail.com>
2021-10-08 15:04:41 +0900

    Add question content

---
+++ pento/master-node.sh
@@ -0,0 +1,4 @@
+# Master node: coredns deployment has image: 'k8s.gcr.io/coredns:1.6.7'
+# Fix kube-apiserver. Make sure its running and healthy.
+# kubeconfig = /root/.kube/config, User = 'kubernetes-admin' Cluster: Server Port = '6443'
+
---
+++ pento/node01.sh
@@ -0,0 +1,2 @@
+# node01 is ready and can schedule pods?
+# node01 has hostPath created = '/web'
