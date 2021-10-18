30a3e63f27bf03cf9d986ed83ae35a727916fae1
Psyrus <revolution.x@gmail.com>
2021-10-18 21:40:33 +0900

    Did most of them

--- pento/master-node.sh
+++ pento/master-node.sh
@@ -2,3 +2,8 @@
 # Fix kube-apiserver. Make sure its running and healthy.
 # kubeconfig = /root/.kube/config, User = 'kubernetes-admin' Cluster: Server Port = '6443'
 
+# Open /etc/kubernetes/manifests/kube-apiserver.yaml
+# Change ca-authority.crt to ca.crt
+sed -i 's/kubernetes\/pki\/ca-authority.crt/kubernetes\/pki\/ca.crt/g'
+# Update deployment coredns in kube-system namespace to have the image:
+## kubectl set image deployment/coredns coredns=k8s.gcr.io/coredns:1.6.7 -n kube-system
--- pento/node01.sh
+++ pento/node01.sh
@@ -1,2 +1,4 @@
 # node01 is ready and can schedule pods?
 # node01 has hostPath created = '/web'
+
+# kubectl uncordon node01
